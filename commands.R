49084f4815749a2ab18194991944a8c30c0bd9ca
marcoheimanntrading <47811980+marcoheimanntrading@users.noreply.github.com>
2019-02-21 09:15:37 +0100

    staging all files

--- commands.R
+++ commands.R
@@ -1,2 +1,5 @@
 require(blogdown)
 #blogdown::new_site(theme = "kakawait/hugo-tranquilpeak-theme")
+
+# Bulk submit on github
+#https://stackoverflow.com/questions/43690977/commit-a-large-number-of-files-in-rstudio-using-git-panel
