d9871c57c45365acd84521eb1104231b5bb0d85e
marcoheimanntrading <marcoheimanntrading@gmail.com>
2019-03-16 11:30:15 +0100

    staging all files

--- commands.R
+++ commands.R
@@ -4,5 +4,15 @@ require(blogdown)
 # Bulk submit on github
 # https://stackoverflow.com/questions/43690977/commit-a-large-number-of-files-in-rstudio-using-git-panel
 # git add -A && git commit -m 'staging all files'
+# 
+# https://stackoverflow.com/questions/11656761/git-please-tell-me-who-you-are-error
+# git init
+# git config user.name "marcoheimanntrading"
+# git config user.email "marcoheimanntrading@gmail.com"
+# git add *
+# git commit -m "some init msg"
+
+# git config --global user.email "marcoheimanntrading@gmail.com" 
+# git config --global user.name "Marco Heimann"
 
 # icons left pane: https://www.w3schools.com/icons/fontawesome_icons_webapp.asp
